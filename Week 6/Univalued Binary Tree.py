eb6a667d250c52b66744e4df591c5acfbd7e2c96
starfish8681 <55570108+starfish8681@users.noreply.github.com>
2019-10-18 10:43:32 +0800

    Update Univalued Binary Tree.py

--- Week 6/Univalued Binary Tree.py
+++ Week 6/Univalued Binary Tree.py
@@ -1,3 +1,6 @@
+#A binary tree is univalued if every node in the tree has the same value.
+#Return true if and only if the given tree is univalued.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -9,11 +12,11 @@ class Solution:
     def isUnivalTree(self, root: TreeNode) -> bool:
         vals = []
 
-        def dfs(node):
+        def dfs(node): #若node非0或Null
             if node:
                 vals.append(node.val)
                 dfs(node.left)
                 dfs(node.right)
 
         dfs(root)
-        return len(set(vals)) == 1
+        return len(set(vals)) == 1  #不重複的數數量是否為1
