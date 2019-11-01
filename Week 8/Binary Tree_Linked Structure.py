ca39ca6c42651063091d46ced2f02d8ee7af9f1a
starfish8681 <55570108+starfish8681@users.noreply.github.com>
2019-11-01 20:29:13 +0800

    Create Binary Tree_Linked Structure.py

---
+++ Week 8/Binary Tree_Linked Structure.py
@@ -0,0 +1,22 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+  
+  def is_left(self):
+    return self.left if self.left != None
+
+  def is_right(self):
+    return self.right if self.right != None
+
+  def replace(self, node):
+    
+
+
+  def add_left(self, node):
+    if self.left == None:
+      self.left = node
+    elif:
+      self.replace(node)
+# class BinaryTree:
